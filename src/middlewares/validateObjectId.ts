53301491e178096f9efd28129c3aa58e5a485a6a
valery-ish <valery-ish@yandex.ru>
2022-09-12 20:46:24 +0300

    error fix

--- src/middlewares/validateObjectId.ts
+++ src/middlewares/validateObjectId.ts
@@ -7,8 +7,8 @@ import mongoose from 'mongoose';
 import { NotFoundError } from '../errors';
 
 const validateObjectId = (req: Request, res: Response, next: NextFunction) => {
-  const { isedId, cardId } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(isedId) || !mongoose.Types.ObjectId.isValid(cardId)) {
+  const { iserId, cardId } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(iserId) || !mongoose.Types.ObjectId.isValid(cardId)) {
     throw new NotFoundError('Данные не найдены');
   }
   next();
